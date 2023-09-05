45ded14313b0ea07bdfa0c420c928f1218258abb
Sigit Fajar Desfian <sigitfajard@gmail.com>
2023-09-05 20:56:02 +0700

    redesign data structure, add button, and reload bug fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('form');
-    const title = document.getElementById('inputBook');
-    const author = document.getElementById('inputAuthor');
-    const unreadList = document.getElementById('unread');
-    const readedList = document.getElementById('readed');
+    const form = document.querySelector('#form');
+    const title = document.querySelector('#inputBook');
+    const author = document.querySelector('#inputAuthor');
+    const unreadList = document.querySelector('#unread');
+    const readedList = document.querySelector('#readed');
+    const dateInput = document.querySelector('#date');
 
     const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
+
+    const RENDER_EVENT = 'render-books';
 
     function updateLocalStorage(books) {
         localStorage.setItem('books', JSON.stringify(books));
@@ -17,12 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
         bookDiv.innerHTML = `
             <div class="book-name">
                 <p class="title">${book.title}</p>
-                <p class="author">by ${book.author}</p>
+                <p class="author">by ${book.author}, ${book.year}</p>
             </div>
             <div class="book-actions">
+                <button class="book-button remove-button">Hapus</button>
                 ${isReaded ?
-                `<button class="book-button redo-button">Ulangi</button>
-                     <button class="book-button remove-button">Hapus</button>` :
+                `<button class="book-button redo-button">Ulangi</button>` :
                 `<button class="book-button done-button">Selesai</button>`
             }
             </div>
@@ -46,84 +49,121 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
 
+        bookDiv.setAttribute('data-id', book.id);
+
         return bookDiv;
-    };
+    }
 
     function markBookAsDone(book) {
-        book.readed = true;
+        book.isComplete = true;
         updateLocalStorage(storedBooks);
 
-        const bookElement = unreadList.querySelector(`[data-title="${book.title}"]`);
+        const bookElement = unreadList.querySelector(`[data-id="${book.id}"]`);
         if (bookElement) {
             readedList.appendChild(createBookElement(book, true));
             bookElement.remove();
         }
-    };
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
 
     function markAsUndone(book) {
-        book.readed = false;
+        book.isComplete = false;
         updateLocalStorage(storedBooks);
-    
-        const bookElement = readedList.querySelector(`[data-title="${book.title}"]`);
+
+        const bookElement = readedList.querySelector(`[data-id="${book.id}"]`);
         if (bookElement) {
             bookElement.remove();
             unreadList.appendChild(createBookElement(book, false));
-        };
-
-        window.location.reload();
-    };
-    
+        }
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
 
     function removeBook(book) {
-        const index = storedBooks.findIndex((b) => b.title === book.title);
+        const index = storedBooks.findIndex((b) => b.id === book.id);
         if (index !== -1) {
             storedBooks.splice(index, 1);
             updateLocalStorage(storedBooks);
 
-            const bookElement = document.querySelector(`[data-title="${book.title}"]`);
+            const bookElement = document.querySelector(`[data-id="${book.id}"]`);
             if (bookElement) {
                 bookElement.remove();
             }
         }
-    };
 
-    for (book of storedBooks) {
-        const isReaded = book.readed;
-        const bookDiv = createBookElement(book, isReaded);
-        bookDiv.setAttribute('data-title', book.title);
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
 
-        if (isReaded) {
-            readedList.appendChild(bookDiv);
-        } else {
-            unreadList.appendChild(bookDiv);
+    function reload() {
+        for (const book of storedBooks) {
+            const isReaded = book.isComplete;
+            const bookDiv = createBookElement(book, isReaded);
+
+            bookDiv.setAttribute('data-id', book.id);
+
+            if (isReaded) {
+                readedList.appendChild(bookDiv);
+            } else {
+                unreadList.appendChild(bookDiv);
+            }
         }
-    };
+    }
+
+    document.addEventListener(RENDER_EVENT, () => {
+        unreadList.innerHTML = '';
+        readedList.innerHTML = '';
+
+        for (const book of storedBooks) {
+            const isReaded = book.isComplete;
+            const bookDiv = createBookElement(book, isReaded);
+
+            bookDiv.setAttribute('data-id', book.id);
+
+            if (isReaded) {
+                readedList.appendChild(bookDiv);
+            } else {
+                unreadList.appendChild(bookDiv);
+            }
+        }
+    });
+
+    function generateId() {
+        return +new Date();
+    }
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         const bookTitle = title.value;
         const bookAuthor = author.value;
+        const dateValue = dateInput.value;
+
+        const year = new Date(dateValue).getFullYear();
 
         if (bookTitle.trim() === '' || bookAuthor.trim() === '') {
-            alert('isi nama buku dan author...');
+            alert('Isi nama buku dan penulis...');
             return;
         }
 
         const newBook = {
+            id: generateId(),
             title: bookTitle,
             author: bookAuthor,
-            readed: false
+            year: year,
+            isComplete: false
         };
 
         title.value = '';
         author.value = '';
+        dateInput.value = '';
 
         const bookDiv = createBookElement(newBook, false);
-        bookDiv.setAttribute('data-title', newBook.title);
         unreadList.appendChild(bookDiv);
 
         storedBooks.push(newBook);
         updateLocalStorage(storedBooks);
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
     });
+
+    reload()
 });
